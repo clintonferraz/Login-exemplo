cae849476352ab404bcb3dff8b50436aae4d8b4a
Clinton <clinton_ferraz@hotmail.com>
2020-09-14 21:05:06 -0300

    get request for the projects management page

--- frontend/src/pages/AdminPanel/AdminPanel.js
+++ frontend/src/pages/AdminPanel/AdminPanel.js
@@ -3,44 +3,55 @@ import api from '../../services/api'
 import './AdminPanel.css';
 
 
-export default () => {
+export default class AdminPanel extends React.Component {
 
-    const loadProjects = async () => {      
+    state = {
+        projeto: []
+    }
 
-        try{
-            const response = await api.get('projetos') ;
+    componentDidMount(){
+        api.get('projetos')
+            .then(response => {
+                const projeto = response.data;
+                this.setState({ projeto });
+            });
+    }
 
-            console.log(response.data);
-           
-            
-            
 
-        } catch {
-            alert("Falha ao carregar projetos.");
+    render() {
+
+
+        const teste = () => {
+            alert('oi');
+
         }
 
-    }
+        return (
+            <div className='admin-panel'>
+                <div className='btn-container'>
+                    <button id='new-project-btn' onClick={teste} >+ Novo Projeto</button>
+                    {/* this.state.projeto.map(projeto => <button key={projeto.id}>{projeto.Ordem}</button>) */}
+    
+                </div>
+                <div className='projects-container'>
+                    {this.state.projeto.map(projeto =>
+                        <div key={projeto.id}>
+                            <div className='projeto'>
+                                {projeto.Titulo}<br/><br/>
+                                {projeto.Descricao}
 
-    const createCards = () => {
-        
-    }
-
-    loadProjects();
-
-    return (
-        <div className='admin-panel'>
-            <div className='btn-container'>
-                <button id='new-project-btn'>Novo Projeto</button>
+                            </div> 
+                        </div>
+                    )}
+    
+    
+    
+                </div>
+    
+    
+    
             </div>
-            <div className='projects-container'>
-
-
-
-
-            </div>
-
-
-
-        </div>
-    )
+        )
+    
+    }
 }
