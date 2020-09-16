c8aabc87754875069631d9b1b3ec21281405c509
Clinton <clinton_ferraz@hotmail.com>
2020-09-15 21:04:59 -0300

    create and delete projects functionality

--- frontend/src/pages/AdminPanel/AdminPanel.js
+++ frontend/src/pages/AdminPanel/AdminPanel.js
@@ -2,53 +2,117 @@ import React from 'react';
 import api from '../../services/api'
 import './AdminPanel.css';
 
-
 export default class AdminPanel extends React.Component {
-
+ 
     state = {
-        projeto: []
+        projeto: [],
+        titulo:'',
+        descricao:'',
+    }
+ 
+    reloadProjects(){
+        api.get('projetos')
+        .then(response => {
+            const projeto = response.data;
+            this.setState({ projeto });
+        });
     }
 
     componentDidMount(){
-        api.get('projetos')
-            .then(response => {
-                const projeto = response.data;
-                this.setState({ projeto });
-            });
+        this.reloadProjects();
     }
 
-
     render() {
+        let Titulo, Descricao;
+
+        const newProject = () => {
+                api.post('projetos', {
+                        Titulo:Titulo,
+                        Descricao:Descricao,
+                    },  
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.tolken}`,
+                        },
+                    }
+                )
+                .then(response => { 
+                    this.reloadProjects();
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                      console.log(error.response.status);
+                      console.log(error.response);
+                      alert('Não foi possível criar o novo projeto, verifique se o titulo já está em uso.');
+                    }
+                });
+                hideNewProjectForm();
+            
+        }
+
+        const showNewProjectForm = () => {
+            document.querySelector('.new-project-form').classList.remove('hidden');
+        }
+        const hideNewProjectForm = () => {
+            document.querySelector('.new-project-form').classList.add('hidden');
+        }
+
+        const logOut = () => {
+            localStorage.clear();
+            window.location.href = '/admin';
 
 
-        const teste = () => {
-            alert('oi');
+        }       
+
+
+
+        const delBtnClick = (projeto) => {
+            api.delete('projetos/' + projeto.id,  
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.tolken}`,
+                    },
+                }
+            )
+            .then(response => { 
+                this.reloadProjects();
+            });
+
+
 
         }
 
         return (
             <div className='admin-panel'>
                 <div className='btn-container'>
-                    <button id='new-project-btn' onClick={teste} >+ Novo Projeto</button>
+                    <button id='new-project-btn' onClick={showNewProjectForm} >+ Novo Projeto</button>
+                    <button id='exit-btn' onClick={logOut}>Sair</button>
                     {/* this.state.projeto.map(projeto => <button key={projeto.id}>{projeto.Ordem}</button>) */}
     
                 </div>
                 <div className='projects-container'>
                     {this.state.projeto.map(projeto =>
-                        <div key={projeto.id}>
-                            <div className='projeto'>
+                       
+                            <div className='projeto' key={projeto.id}>
                                 {projeto.Titulo}<br/><br/>
                                 {projeto.Descricao}
-
+                                <button onClick={() => delBtnClick(projeto)}>Delete</button>
                             </div> 
-                        </div>
+                        
                     )}
-    
-    
-    
                 </div>
-    
-    
+                <div className='new-project-form hidden'>
+                    Título:
+                    <input type='text' onChange={event => Titulo = event.target.value}></input>
+                    Descrição:
+                    <textarea rows='4' onChange={event => Descricao = event.target.value}></textarea>
+                    <div className='buttons'>
+                        <button onClick={newProject}>Criar Projeto</button>
+                        <button onClick={hideNewProjectForm}>Cancelar</button>
+                    </div>
+                    
+                </div>
     
             </div>
         )
